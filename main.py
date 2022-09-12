d888b6ed47d685db5473be6701f32ff6878e37cf
Cinklerm <112488498+Cinklerm@users.noreply.github.com>
2022-09-12 19:50:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 def count_word(chaine):
+    # On vérifie si le paramètre est vide
     if chaine is None:
         return 0
+    # On fait le délimitage de la chaine avec split
+    # On utilise le délimiteur par défaut pour éviter un cas avec plusieurs délimiteur consecutifs
+    # https://python-reference.readthedocs.io/en/latest/docs/str/split.html
     count= len(chaine.split());
     return count
